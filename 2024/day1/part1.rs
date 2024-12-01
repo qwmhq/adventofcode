a5e5c82a52979c13dcf3a2ef2c1ed85766ae36c3
Quwam Ogunlaja <quwam32@gmail.com>
2024-12-01 12:29:14 +0100

    day 1

---
+++ 2024/day1/part1.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let path = "input";
+    let mut left_list = Vec::new();
+    let mut right_list = Vec::new();
+    let mut distance_sum = 0;
+
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        let values: Vec<u64> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse::<u64>().ok())
+            .collect();
+        left_list.push(values[0]);
+        right_list.push(values[1]);
+    }
+
+    left_list.sort();
+    right_list.sort();
+
+    for i in 0..left_list.len() {
+        let dist = (left_list[i] as i64 - right_list[i] as i64).unsigned_abs();
+        distance_sum += dist;
+    }
+
+    println!("sum of distances: {distance_sum}");
+
+    Ok(())
+}
---
+++ 2024/day1/part2.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let path = "input";
+    let mut left_list = Vec::new();
+    let mut right_dict = HashMap::new();
+    let mut similarity_score = 0;
+
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        let values: Vec<u64> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse::<u64>().ok())
+            .collect();
+        left_list.push(values[0]);
+        right_dict
+            .entry(values[1])
+            .and_modify(|x| *x += 1)
+            .or_insert(1);
+    }
+
+    for i in left_list {
+        if let Some(x) = right_dict.get(&i) {
+            similarity_score += x * i;
+        }
+    }
+
+    println!("similarity score: {similarity_score}");
+
+    Ok(())
+}
